f340cdd5b2089ece5f56399d73e7f2bddc362864
Muhammed AbuOdeh <mra9407@nyu.edu>
2023-11-17 14:10:19 +0400

    improved code naming

--- src/dependency_parser/biaff_parser.py
+++ src/dependency_parser/biaff_parser.py
@@ -1,21 +1,30 @@
 from typing import List, Union, Dict
 
 from supar import Parser
+from supar.utils import Dataset
 
 from camel_tools.utils.dediac import dediac_ar
 
 import time
 
-def conll_to_parsed_tuples(conll):
-    parsed_tuples = []
-    for sent in conll:
-        sentence = []
-        for i in range(len(sent.values[0])):
-            word = [col[i] for col in sent.values]
-            sentence.append(tuple(word))
-        parsed_tuples.append(sentence)
+"""
+conll object from parser
+iterate over sentences from this object
+iterate over the columns per sentence
+create token rows
+create conll sentence of token row tuples
 
-    return parsed_tuples
+"""
+
+def parser_conll_to_conll_tuples(parser_conll: Dataset) -> List[List[tuple]]:
+    conll_sentences = []
+    for parser_sentence in parser_conll:
+        sentence_tree_token_tuples = []
+        for i in range(len(parser_sentence.values[0])):
+            token_tuple_row = tuple(column[i] for column in parser_sentence.values)
+            sentence_tree_token_tuples.append(token_tuple_row)
+        conll_sentences.append(sentence_tree_token_tuples)
+    return conll_sentences
 
 def filter_tatweel(form):
     if form.replace("_", "").replace("\u0640","").replace("\u005F", "") == "":
@@ -41,10 +50,10 @@ def parse_text_tuples(sentence_tuples: List[List[tuple]], parse_model, logs: Dic
     sentence_tuples = [[val[1:4] for val in sent] for sent in sentence_tuples]
     form_lemma_pos_tuple = [[(filter_tatweel(dediac_ar(val[0])), filter_tatweel(dediac_ar(val[1])), val[2]) for val in sent] for sent in sentence_tuples]
     conll = parse(form_lemma_pos_tuple, parse_model=parse_model, logs = logs)
-    return conll_to_parsed_tuples(conll)
+    return parser_conll_to_conll_tuples(conll)
 
 def parse_conll(conll_path: str, parse_model, logs: Dict[str, float] = None) -> List[List[tuple]]:
     conll = parse(conll_path, parse_model=parse_model, logs=logs)
     for i, sent in enumerate(conll):
         conll[i].values[1] = [filter_tatweel(form) for form in sent.values[1]]
-    return conll_to_parsed_tuples(conll)
+    return parser_conll_to_conll_tuples(conll)
