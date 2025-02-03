c7e7d5b09b4e0dc4543f6c2ee1d251c6367bd1ef
Muhammed AbuOdeh <mra9407@nyu.edu>
2025-02-03 13:45:21 +0400

    formatting

--- server_code.py
+++ server_code.py
@@ -15,13 +15,15 @@ from camel_tools.utils.charmap import CharMapper
 
 from flask_cors import CORS
 import sys
-sys.path.insert(0,'camel_parser/src')
+
+sys.path.insert(0, "camel_parser/src")
 
 from dotenv import load_dotenv
-load_dotenv('.env')
+
+load_dotenv(".env")
 
 
-project_dir = os.path.expanduser('~/palmyra_server/palmyra_server')
+project_dir = os.path.expanduser("~/palmyra_server/palmyra_server")
 # for local dev
 # project_dir = os.path.expanduser('.')
 
@@ -31,8 +33,9 @@ arclean = CharMapper.builtin_mapper("arclean")
 #
 ### Get clitic features
 #
-clitic_feats_df = read_csv(f'{project_dir}/camel_parser/data/clitic_feats.csv')
-clitic_feats_df = clitic_feats_df.astype(str).astype(object) # so ints read are treated as string objects
+clitic_feats_df = read_csv(f"{project_dir}/camel_parser/data/clitic_feats.csv")
+# so ints read are treated as string objects
+clitic_feats_df = clitic_feats_df.astype(str).astype(object)
 
 
 # disambiguator = get_disambiguator("bert", "calima-msa-s31")
@@ -44,9 +47,9 @@ CLIENT_SECRETS_FILE = f"{os.path.expanduser(project_dir)}/client_secret.json"
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
 # os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-API_SERVICE_NAME = 'drive'
-API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+API_SERVICE_NAME = "drive"
+API_VERSION = "v3"
 
 PARSE_WORD_LIMIT = 100
 
@@ -54,7 +57,7 @@ app = flask.Flask(__name__)
 # Note: A secret key is included in the sample so that it works.
 # If you use this code in your application, replace this with a truly secret
 # key. See https://flask.palletsprojects.com/quickstart/#sessions.
-app.secret_key = os.getenv('FLASK_SECRET')
+app.secret_key = os.getenv("FLASK_SECRET")
 
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -63,82 +66,91 @@ cors = CORS(app, supports_credentials=True)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-@app.route('/parse_data', methods=['POST'])
+@app.route("/parse_data", methods=["POST"])
 def parse_data():
-  all_lines = request.get_json()['sentences']
-  parser_type = request.get_json()['parserType']
-  file_type = 'text'
+    all_lines = request.get_json()["sentences"]
+    parser_type = request.get_json()["parserType"]
+    file_type = "text"
 
-  if parser_type == "ar_catib":
-    parser_model_name = "CAMeLBERT-CATiB-biaffine.model"
-    tagset = "catib6"
-  elif parser_type == "ar_ud":
-    parser_model_name = "CAMeLBERT-UD-biaffine.model"
-    tagset = "ud"
-  else:
-    # just in case user messes with html
-    return
+    if parser_type == "ar_catib":
+        parser_model_name = "CAMeLBERT-CATiB-biaffine.model"
+        tagset = "catib6"
+    elif parser_type == "ar_ud":
+        parser_model_name = "CAMeLBERT-UD-biaffine.model"
+        tagset = "ud"
+    else:
+        # just in case user messes with html
+        return
 
-  disambiguator = get_disambiguator("mle", "calima-msa-s31")
+    disambiguator = get_disambiguator("mle", "calima-msa-s31")
 
-  # will parse lines for a total of 100 words
-  lines, lines_to_ignore = get_lines_to_parse(all_lines, PARSE_WORD_LIMIT)
+    # will parse lines for a total of 100 words
+    lines, lines_to_ignore = get_lines_to_parse(all_lines, PARSE_WORD_LIMIT)
 
-  file_type_params = get_file_type_params(lines, file_type, '', f'{project_dir}/camel_parser/models/{parser_model_name}',
-      arclean, disambiguator, clitic_feats_df, tagset, 'calima-msa-s31')
-  parsed_text_tuples = parse_text(file_type, file_type_params)
+    file_type_params = get_file_type_params(
+        lines,
+        file_type,
+        "",
+        f"{project_dir}/camel_parser/models/{parser_model_name}",
+        arclean,
+        disambiguator,
+        clitic_feats_df,
+        tagset,
+        "calima-msa-s31",
+    )
+    parsed_text_tuples = parse_text(file_type, file_type_params)
 
-  string_lines = text_tuples_to_string(parsed_text_tuples, sentences=lines)
+    string_lines = text_tuples_to_string(parsed_text_tuples, sentences=lines)
 
-  # add parsed lines to unparsed lines
-  if lines_to_ignore:
-    lines_to_ignore = unparsed_lines_to_conll(lines_to_ignore)
-    string_lines = string_lines + lines_to_ignore
+    # add parsed lines to unparsed lines
+    if lines_to_ignore:
+        lines_to_ignore = unparsed_lines_to_conll(lines_to_ignore)
+        string_lines = string_lines + lines_to_ignore
 
-  parsed_data = '\n'.join(string_lines)
+    parsed_data = "\n".join(string_lines)
 
-  new_id = datetime.datetime.now().strftime('%s') + str(int(random.random()*100000))
+    new_id = datetime.datetime.now().strftime("%s") + str(int(random.random() * 100000))
 
-  with open(f'{project_dir}/data/temp_parsed/{new_id}', 'w') as f:
-    f.write(parsed_data)
+    with open(f"{project_dir}/data/temp_parsed/{new_id}", "w") as f:
+        f.write(parsed_data)
 
-  return new_id
+    return new_id
 
 
-@app.route('/get_parsed_data', methods=['GET'])
+@app.route("/get_parsed_data", methods=["GET"])
 def get_parsed_data():
-  data_id = request.args.get("data_id")
-  conll_file_path = f'{project_dir}/data/temp_parsed/{data_id}'
+    data_id = request.args.get("data_id")
+    conll_file_path = f"{project_dir}/data/temp_parsed/{data_id}"
 
-  data = []
-  with open(conll_file_path, 'r') as f:
-    data = f.readlines()
-  os.remove(conll_file_path)
+    data = []
+    with open(conll_file_path, "r") as f:
+        data = f.readlines()
+    os.remove(conll_file_path)
 
-  return ''.join(data)
+    return "".join(data)
 
-@app.route('/get_gapi_credentials', methods=['GET'])
+
+@app.route("/get_gapi_credentials", methods=["GET"])
 def get_gapi_credentials():
-  return {
-    'apiKey': os.getenv('GCP_API_KEY'),
-    'clientId': os.getenv('GCP_CLIENT_ID'),
-    'discovertDocs': [os.getenv('GCP_DISCOVERY_DOC')]
-  }
+    return {
+        "apiKey": os.getenv("GCP_API_KEY"),
+        "clientId": os.getenv("GCP_CLIENT_ID"),
+        "discovertDocs": [os.getenv("GCP_DISCOVERY_DOC")],
+    }
 
-@app.route('/get_gis_credentials', methods=['GET'])
+
+@app.route("/get_gis_credentials", methods=["GET"])
 def get_gis_credentials():
-  return {
-    'client_id': os.getenv('GCP_CLIENT_ID'),
-    'scope': SCOPES
-  }
+    return {"client_id": os.getenv("GCP_CLIENT_ID"), "scope": SCOPES}
 
-if __name__ == '__main__':
-  project_dir = os.path.expanduser('.')
-  # When running locally, disable OAuthlib's HTTPs verification.
-  # ACTION ITEM for developers:
-  #     When running in production *do not* leave this option enabled.
-  # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-  # Specify a hostname and port that are set as a valid redirect URI
-  # for your API project in the Google API Console.
-  # app.run('localhost', 8080, debug=True)
+if __name__ == "__main__":
+    project_dir = os.path.expanduser(".")
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    # app.run('localhost', 8080, debug=True)
